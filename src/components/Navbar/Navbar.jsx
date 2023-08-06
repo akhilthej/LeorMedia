15e4d3ce40115c57d7f2b7b8653f2a1080695e68
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-08-06 15:37:07 +0530

    navmob

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Menu, Transition } from '@headlessui/react';
 
 function classNames(...classes) {
@@ -8,16 +8,22 @@ function classNames(...classes) {
 
 const Navbar = () => {
   return (
-    <main class ='mt-1 mx-4 sm:mx-20 rounded-2xl sticky top-0 z-50 bg-white -mb-20 py-2' >
-    <div className=' w-full h-14 flex justify-between items-center px-10 md:px-15 text-black '>
-      <Link to='/'><img class="h-14 w-auto" src={require('../../assets/Common/LEORMEDIA_navbarlogo.png')} alt='Leormedia_logo' /></Link>
-      
-      <div class=" justify-end ">
-      <ul className='flex items-center text-sm'>
-      <li className='p-4 '><Link to='/aboutus'>About</Link></li>
-        <li className='p-4 '><Link to='/digital-creative-media'>Digital Creative Media</Link></li>
+    <main className="mt-1 mx-4 sm:mx-0 md:mx-20 rounded-2xl sticky top-0 z-50 bg-white -mb-20 py-2">
+      <div className="w-full h-14 flex justify-between items-center px-4 sm:px-10 md:px-15 text-black">
+        <Link to="/">
+          <img
+            className="h-10 w-auto"
+            src={require('../../assets/Common/LEORMEDIA_navbarlogo.png')}
+            alt="Leormedia_logo"
+          />
+        </Link>
 
-        <Menu as='div' className='relative inline-block text-left '>
+        <div className="flex justify-end">
+          <ul className="flex items-center text-sm">
+            <li className="p-4 hidden md:block"> <Link to="/aboutus">About</Link></li>
+            <li className="p-4 hidden md:block"> <Link to="/digital-creative-media">Digital Creative Media</Link></li>
+
+        <Menu as='div' className='relative  text-left hidden md:inline-block'>
             <div>
               <Menu.Button className='flex justify-center p-4'>
                 Branding
@@ -140,7 +146,7 @@ const Navbar = () => {
         </Menu>
 
         
-        <Menu as='div' className='relative inline-block text-left '>
+        <Menu as='div' className='relative  text-left hidden md:inline-block'>
             <div>
               <Menu.Button className='flex justify-center p-4'>
                 Events
@@ -174,7 +180,7 @@ const Navbar = () => {
                   </Menu.Item>
 
                   <Menu as='div'>
-              <Menu.Button className='flex justify-center p-4 text-white'>
+              <Menu.Button className='flex justify-center p-4 text-white '>
               Wedding
                 <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 mt-0.5 ml-1">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -189,7 +195,7 @@ const Navbar = () => {
               leave='transition ease-in duration-75'
               leaveFrom='transform opacity-100 scale-100'
               leaveTo='transform opacity-0 scale-95' >
-              <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-gray-800 ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
+              <Menu.Items className='origin-top-right absolute right-0   rounded-md shadow-lg bg-gray-800 ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
                   <Menu.Item>
                     {({ active }) => (
                       <Link to='/wedding-planners'><a
@@ -316,7 +322,7 @@ const Navbar = () => {
           </Menu>
 
 
-        <Menu as='div' className='relative inline-block text-left '>
+        <Menu as='div' className='relative  text-left hidden md:inline-block'>
             <div>
               <Menu.Button className='flex justify-center p-4'>
                 Advertising
@@ -334,7 +340,7 @@ const Navbar = () => {
               leave='transition ease-in duration-75'
               leaveFrom='transform opacity-100 scale-100'
               leaveTo='transform opacity-0 scale-95' >
-              <Menu.Items className='origin-top-right absolute right-0 mt-3 w-56 rounded-md shadow-lg bg-black ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
+              <Menu.Items className='origin-top-right absolute right-0 mt-3 w-24 rounded-md shadow-lg bg-black ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
                   <Menu.Item>
                     {({ active }) => (
                       <Link to='/atl-advertising'><a
@@ -366,9 +372,149 @@ const Navbar = () => {
             </Transition>
           </Menu>
         
-        <li className='p-4 '><Link to='/contact'>Contact us</Link></li>
-     
+        <li className='p-4 hidden md:block'><Link to='/contact'>Contact us</Link></li>
+
+
+
+
+        <li className='p-4 block md:hidden'>
+        <Menu as='div' className='relative inline-block text-left '>
+            <div>
+              <Menu.Button className='inline-flex justify-center w-full rounded-md border border-gray-900 shadow-sm px-4 py-2 bg-white text-sm font-medium text-black hover:text-white  hover:bg-black focus:outline-none  '>
+                More
+                <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 mt-0.5 ml-1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-900 focus:outline-none z-50'>
+
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link to='/digital-creative-media'><a
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                      Digital Creative Media
+                        
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+                  
+                  <Menu.Item >
+                    {({ active }) => (
+                      <Link to='/getquotation'><a
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                      Branding
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item >
+                    {({ active }) => (
+                      <Link to='/clients'><a
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                      Clients
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+            
+              
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link to='/technologies'><a
+                      
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Technologies
+                        
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link to='/reviews'><a
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Reviews
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+                  
+                  
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link to='/aboutus'><a
+
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        About us
+                     
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+
+                  <Menu.Item >
+                    {({ active }) => (
+                      <Link to='/contact'><a
+                        className={classNames(
+                          active
+                            ? 'bg-black text-white'
+                            : 'text-black',
+                          'block px-4 py-2 text-sm'
+                        )} >
+                       Contact us
+                      </a></Link>
+                    )}
+                  </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </li>
       </ul>
+
+      
     </div>
     </div>
     </main>
