8f00ec491486c8456a47c132b0648bcc2bb0ce9c
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-10-17 11:15:56 +0530

    home screen

--- src/components/data/leordata.jsx
+++ src/components/data/leordata.jsx
@@ -2,15 +2,32 @@ import LeormediaLogo from '../../assets/Common/LeormediaLogo.png';
 
 import HeroCoverBG from '../../assets/Common/HeroCoverBG.png';
 import googlereview from '../../assets/Icons/googlereview.png';
-import WhatsApp from '../../assets/Icons/WhatsApp.png';
+import WhatsApp from '../../assets/Icons/WhatsApp.webp';
 
 /* icons */
-import DigitalCreativeMedia from '../../assets/Icons/DigitalCreativeMedia.png';
-import EventManagement from '../../assets/Icons/EventManagement.png';
-import Branding from '../../assets/Icons/Branding.png';
-import Advertising from '../../assets/Icons/Advertising.png'
+import DigitalCreativeMedia from '../../assets/Icons/DigitalMarketing.svg';
+import EventManagement from '../../assets/Icons/GraphicDesign.svg';
+import Branding from '../../assets/Icons/Branding.svg';
+import Advertising from '../../assets/Icons/Advertising.svg';
+
+
+/*HomePage*/
+import documentationicon from '../../assets/Icons/documentation-icon.png'
+import analysisicon from '../../assets/Icons/analysis-icon.png'
+import socialicon from '../../assets/Icons/social-icon.png'
+import financeicon from '../../assets/Icons/finance-icon.png'
+import statergyicon from '../../assets/Icons/Statergy-icon.png'
+import businessreporticon from '../../assets/Icons/business-report-icon.png'
+import FixedPrice from '../../assets/Icons/FixedPrice.svg'
+import TimeManage from '../../assets/Icons/TimeManage.svg'
+import OntimeDelivery from '../../assets/Icons/OntimeDelivery.svg'
+import HiringModel from '../../assets/Icons/HiringModel.svg'
 
 export {
+
+  /*HomePage*/
+  
+  documentationicon,analysisicon,socialicon,financeicon,statergyicon,businessreporticon,FixedPrice,TimeManage,OntimeDelivery,HiringModel,
 
     /* Common */
     LeormediaLogo,HeroCoverBG,googlereview,WhatsApp,
